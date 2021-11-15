cc62e6d118856bb4ad3e9cd973ac555f8bf83632
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-15 11:34:15 +0000

    Renamed to movie showtimes

--- __test__/upcoming_movies.test.js
+++
@@ -1,9 +0,0 @@
-const upcomingMovies = require('../lib/upcoming_movies');
-
-// Add your test suites here
-describe('', () => {
-
-  test('should ', () => {
-    expect(upcomingMovies()).toBe(0);
-  });
-});
---
+++ __tests__/upcoming_movies.test.js
@@ -0,0 +1,9 @@
+const movieShowtimes = require('../lib/movie_showtimes');
+
+// Add your test suites here
+describe('movieShowtimes', () => {
+
+  test('should ', () => {
+    expect(movieShowtimes()).toBe(0);
+  });
+});
