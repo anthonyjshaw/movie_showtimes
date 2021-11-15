1585cf6813462a696e937f1a2bf66c10fd3fa68b
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-15 11:30:01 +0000

    Initialized project

---
+++ __test__/upcoming_movies.test.js
@@ -0,0 +1,9 @@
+const upcomingMovies = require('../lib/upcoming_movies');
+
+// Add your test suites here
+describe('', () => {
+
+  test('should ', () => {
+    expect(upcomingMovies()).toBe(0);
+  });
+});
